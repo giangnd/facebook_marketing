a6351a736ba973304f3a47f0187423742db48b77
Nguyễn Đình Giang <dinhgiangbg91@gmail.com>
2018-04-23 18:05:10 +0700

    commit

--- models/Counter.js
+++ models/Counter.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const CounterSchema = mongoose.Schema({
+var CounterSchema = mongoose.Schema({
   _id: { type: String, required: true },
   seq: { type: Number, default: 1000 },
 });
--- models/Shop.js
+++ models/Shop.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Counter = require('./Counter');
 
 
-const Shop = mongoose.Schema({
+var Shop = mongoose.Schema({
   shopId: Number,
   shopify_domain: String, // Shopify domain without the .myshopify.com on the end.
   name: String,
