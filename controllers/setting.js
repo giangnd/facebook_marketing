6607d5064ccf7f48a8832981c38041d55df28f7a
Nguyễn Đình Giang <dinhgiangbg91@gmail.com>
2018-05-02 13:56:09 +0700

    commit

--- controllers/setting.js
+++ controllers/setting.js
@@ -4,13 +4,14 @@ const Promise = require('bluebird');
 function defineSetting(shop) {
     return new Promise((resolve, reject) => {
         const fbAppId = '283604478840722';
-        
+
         const query = Setting.findOne({ shopify_domain: shop }).exec();
         query.then((response) => {
-            if(!response.fbAppId){
-                response.fbAppId = fbAppId;
+            const setting = response;
+            if (!setting || !setting.fbAppId) {
+                setting.fbAppId = fbAppId;
             }
-            resolve(response);
+            resolve(setting);
         });
     });
 }
